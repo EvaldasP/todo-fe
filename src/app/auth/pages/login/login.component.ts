e788528a8cc8e7e0ba003cd013e76badae8591b4
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-10 15:33:37 +0300

    login form and layout

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public readonly loginForm = this._fb.group({
+    username: this._fb.control('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    password: this._fb.control('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
 
+  constructor(private readonly _fb: FormBuilder) {}
+
+  get isFormInvalid(): boolean {
+    return this.loginForm?.invalid;
+  }
+
+  public onLogin(): void {
+    if (this.isFormInvalid) {
+      return;
+    }
+    const payload = this.loginForm.getRawValue();
+
+    console.log(payload);
+  }
 }
