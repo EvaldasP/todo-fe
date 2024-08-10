8a521a656c3cbbf7c7e3390890d93965cc3543af
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-10 18:42:05 +0300

    initial todo add form and list components

--- src/app/features/todo/todo.module.ts
+++ src/app/features/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { TodoCardComponent } from './pages/todo-list/components/todo-card/todo-card.component';
+import { TodoFormComponent } from './pages/todo-list/components/todo-form/todo-form.component';
 
 @NgModule({
-  declarations: [TodoListComponent],
+  declarations: [TodoListComponent, TodoCardComponent, TodoFormComponent],
   imports: [CommonModule],
   exports: [],
   providers: [],
