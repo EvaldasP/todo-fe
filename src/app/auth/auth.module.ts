dad60feb73627079f132204f937011261fa9956a
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-10 14:29:24 +0300

    initial login component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './pages/login/login.component';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [],
+  exports: [],
+  providers: [],
+})
+export class AuthModule {}
