f74b9d9d73177f01db287900114bc921647cd1a3
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-10 15:05:44 +0300

    base login layout

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './pages/login/login.component';
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule],
   exports: [],
   providers: [],
 })
