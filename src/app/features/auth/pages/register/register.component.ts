31f93bcd6ea3f9922838c0544b606d943a7d708c
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-14 19:19:59 +0300

    register form layout and form

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  public readonly registerForm = this._fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
 
+  constructor(private readonly _fb: NonNullableFormBuilder) {}
+
+  get isFormValid(): boolean {
+    return this.registerForm.valid;
+  }
 }
